ea3d13a6db075e819afc6d8b18d52e97435b6e40
btnguyen2k <btnguyen2k@gmail.com>
2012-10-15 02:40:17 +0700

    * Add age column

--- js/dojostart.js
+++ js/dojostart.js
@@ -9,6 +9,12 @@ function createGrid(store, divId) {
         {name: 'First Name', field: 'firstName', width: '80px'},
         {name: 'Last Name' , field: 'lastName', width: '80px'},
         {name: 'Birthday'  , field: 'birthday'},
+        {name: 'Age'       , field: 'birthday', width: '30px', formatter: function(birthday) {
+                var myDate = dojo.date.locale.parse(birthday, {datePattern:"yyyy/MM/dd", selector: 'date'});
+                var now = new Date();
+                return (now.getYear() - myDate.getYear());
+            }
+        },
         {name: 'Address'   , field: 'address', width: '230px'},
         {name: 'Phone'     , field: 'phone', width: 'auto'},
         {name: 'Actions'   , field: 'id', width: 'auto', formatter: function(id) {
@@ -23,7 +29,7 @@ function createGrid(store, divId) {
         store: store,
         structure: layout,
         rowSelector: '20px',
-        canSort: function(colIndex, field) {return colIndex != 7;}, //do not sort the "Actions" column
+        canSort: function(colIndex, field) {return colIndex != 8 && colIndex != 5;}, //do not sort the "Actions" and "Age" columns
         plugins : {
             filter: {
                 // Show the closeFilterbarButton at the filter bar
